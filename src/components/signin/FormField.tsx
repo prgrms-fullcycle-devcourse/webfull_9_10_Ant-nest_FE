1797348cfe36addcae4a9ee707ff8b2185890e25
Jelly <skguswl8038@naver.com>
2026-03-21 04:47:44 +0900

    SCRUM-7 feat: 로그인 화면 퍼블리싱
    1. 파비콘 추가
    2. gif 추가
    3. 컬러 추가
    4. radix 아이콘 추가
    5. radix override 파일 추가
    6. 모바일 테스트를 위한 dev 서버 host 설정 추가

---
+++ src/components/signin/FormField.tsx
@@ -0,0 +1,41 @@
+import {TextField, IconButton} from "@radix-ui/themes";
+import {useState} from "react";
+import {EyeClosedIcon, EyeOpenIcon} from "@radix-ui/react-icons";
+
+
+interface FormFieldProps {
+    className?: string;
+    label: string;
+    helpText?: string;
+    value: string | number;                    // 입력 데이터
+    placeholder: string;
+    type?: 'number' | 'email' | 'password';
+    onChange: (value: string) => void; // 데이터 넘겨주기
+    error?: string;// 타입체크 에러메시지
+}
+
+export default function FormField({ className, label, helpText, value, placeholder, type, onChange, error }: FormFieldProps) {
+    const [show, setShow] = useState(false)
+
+    return (
+        <div className={className ?? ''}>
+            <b className="pl-[0.8rem] text-base text-[var(--color-text-default)]">{label}</b>
+            <TextField.Root
+                className="relative mt-[0.5rem] [&.rt-TextFieldRoot]:!h-[3.7rem] [&>input]:!py-0 [&>input]:!pr-[0.8rem] [&>input]:!pl-[1.4rem] input-shadow"
+                value={value}
+                placeholder={placeholder}
+                type={type === 'password' ? (show ? 'text' : 'password') : type}
+                onChange={(e) => onChange(e.target.value)}
+            >
+                {type === "password" && (
+                    <TextField.Slot side="right">
+                        <IconButton type="button" className="!relative !right-[0.6rem] [&:hover]:!bg-transparent" variant="ghost" onClick={() => setShow(!show)}>
+                            {show ? <EyeClosedIcon className="w-[1.4rem] h-[2rem] text-[var(--color-gray)]"/> : <EyeOpenIcon className="w-[1.4rem] h-[2rem] text-[var(--color-gray)]"/>}
+                        </IconButton>
+                    </TextField.Slot>
+                ) as React.ReactNode}
+            </TextField.Root>
+            <p>{error || helpText}</p>
+        </div>
+    );
+}
