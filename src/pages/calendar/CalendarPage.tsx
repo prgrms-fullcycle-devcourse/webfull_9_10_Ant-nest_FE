a1182818cf5722d5ac627fa8d74591243c1c5cc9
Jelly <skguswl8038@naver.com>
2026-03-31 17:47:13 +0900

    SCRUM-36 feat: 캘린더 퍼블리싱
    - 캘린더 컴포넌트 추가
    - 캘린더 헤더 공통 컴포넌트 추가
    - 캘린더 카드 컴포넌트 추가
    - 캘린더 타입 추가
    - 캘린더 스타일 추가
    - 테마 컬러 추가

--- src/pages/calendar/CalendarPage.tsx
+++ src/pages/calendar/CalendarPage.tsx
@@ -1,4 +1,5 @@
-import DiaryCalendar from '@/features/calendar/DiaryCalendar.tsx';
+import DiaryCalendar from '@/features/calendar/components/DiaryCalendar.tsx';
+import CalendarDiaryCard from '@/features/calendar/components/CalendarDiaryCard.tsx';
 
 /** 컴포넌트 **/
 
@@ -6,6 +7,9 @@ export default function CalendarPage() {
   return (
     <div>
       <DiaryCalendar />
+      <div className="pt-[2rem] px-[1rem]">
+        <CalendarDiaryCard />
+      </div>
     </div>
   );
 }
