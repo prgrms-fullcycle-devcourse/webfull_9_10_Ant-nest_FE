7c1edb61c3b3a165d8c54231349de4ded4824d65
Jelly <skguswl8038@naver.com>
2026-04-15 19:17:11 +0900

    SCRUM-9 feat: 로그아웃 시 쿼리 초기화

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,14 +1,17 @@
 import { useNavigate } from 'react-router-dom';
 import { useAuthStore } from '@/store/authStore.ts';
+import { useQueryClient } from '@tanstack/react-query';
 
 /**
  * 로그아웃 훅
  **/
 export const useLogoutMutation = () => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const { logout } = useAuthStore();
 
   return () => {
+    queryClient.clear();
     logout();
     navigate('/login', { replace: true });
   };
